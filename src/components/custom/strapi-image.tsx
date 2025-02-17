fba74175a41c1a6afa0cc3786ff6f82931a318ca
Yongzhen (Larry) Jiang <yongzhen.jiang@gmail.com>
2025-02-17 14:32:37 +1300

    frontend code commit

---
+++ src/components/custom/strapi-image.tsx
@@ -0,0 +1,35 @@
+import Image from "next/image";
+import { getStrapiMedia } from "@/lib/utils";
+
+
+interface StrapiImageProps {
+    src: string;
+    alt: string;
+    height: number;
+    width: number;
+    className?: string;
+    priority: boolean | undefined;
+}
+
+export function StrapiImage({
+    src,
+    alt,
+    height,
+    width,
+    className,
+    priority
+}: Readonly<StrapiImageProps>) {
+    const imageUrl = getStrapiMedia(src);
+    if (!imageUrl) return null;
+
+    return (
+        <Image
+            src={imageUrl}
+            alt={alt}
+            height={height}
+            width={width}
+            className={className}
+            priority={priority ? true : false}
+        />
+    );
+}
---
+++ src/components/forms/signup-form.tsx
@@ -0,0 +1,76 @@
+"use client";
+
+import Link from "next/link";
+import { useActionState } from "react";
+import { registerUserAction } from "@/data/actions/auth-actions";
+import {
+    CardTitle,
+    CardDescription,
+    CardHeader,
+    CardContent,
+    Card,
+    CardFooter
+} from "@/components/ui/card";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+
+
+const INITIAL_STATE = {
+    data: null,
+}
+
+export function SignUpForm() {
+    const [formState, formAction] = useActionState(registerUserAction, INITIAL_STATE);
+    return (
+        <div className="w-full max-w-md">
+            <form action={formAction}>
+                <Card>
+                    <CardHeader className="space-y-1">
+                        <CardTitle className="text-3xl font-bold">Sign Up</CardTitle>
+                        <CardDescription>
+                            Enter your details to create a new account
+                        </CardDescription>
+                    </CardHeader>
+                    <CardContent className="space-y-4">
+                        <div className="space-y-2">
+                            <Label htmlFor="username">Username</Label>
+                            <Input
+                                id="username"
+                                name="username"
+                                type="text"
+                                placeholder="username"
+                            />
+                        </div>
+                        <div className="space-y-2">
+                            <Label htmlFor="email">Email</Label>
+                            <Input
+                                id="email"
+                                name="email"
+                                type="email"
+                                placeholder="name@example.com"
+                            />
+                        </div>
+                        <div className="space-y-2">
+                            <Label htmlFor="password">Password</Label>
+                            <Input
+                                id="password"
+                                name="password"
+                                type="password"
+                                placeholder="password"
+                            />
+                        </div>
+                    </CardContent>
+                    <CardFooter className="flex flex-col">
+                        <button className="w-full">Sign Up</button>
+                    </CardFooter>
+                </Card>
+                <div className="mt-4 text-center text-sm">
+                    Have an account?
+                    <Link className="underline ml-2" href="signin">
+                        Sign In
+                    </Link>
+                </div>
+            </form>
+        </div>
+    )
+}
